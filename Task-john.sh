eabc61c4ad7cddf1eef968eefca752d5c96b5453
Oleksandr-Herbut <168016849+Oleksandr-Herbut@users.noreply.github.com>
2024-05-15 20:26:07 +0200

    Update Task-john.sh
    Script changed

--- Task-john.sh
+++ Task-john.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-echo "My script"
+mkdir -p /opt/040324-wde/john
+cd /opt/040324-wde/john
+
+file_path="/opt/040324-wde/Name/MyTest"
+for i in {1..10}; do
+    date +%T >> "\$file_path"
+    sleep 5
+done
+
+cat "\$file_path"
+
+
